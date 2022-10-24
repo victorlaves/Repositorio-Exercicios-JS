e3615659187d3dd81c59ec857ed4d8e7e9e711a0
Victor Laves <victorpf2020@gmail.com>
2022-10-24 19:37:12 -0300

    Exercicio 1

---
+++ exerciciofds1.js
@@ -0,0 +1,16 @@
+var array = []
+var arrayTrueOrFalse = []
+
+for (i = 0; i < 5; i++) {
+    array[i] = parseInt(prompt("Digite um numero Positivo ou Negativo:"))
+
+    if(array[i] > 0)
+    arrayTrueOrFalse[i] = true
+    else
+    arrayTrueOrFalse[i] = false
+}
+console.log(array)
+console.log(arrayTrueOrFalse)
+
+
+
